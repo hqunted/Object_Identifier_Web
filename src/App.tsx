37f8168781c7279f3857be45b9303643e03e645f
hqunted <berkekirci@gmail.com>
2023-04-06 15:00:03 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedImage(e.target.files[0]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-8">Image Uploader</h1>
+      {selectedImage ? (
+        <div className="flex flex-col items-center">
+          <img
+            src={URL.createObjectURL(selectedImage)}
+            alt="Uploaded Image"
+            className="max-w-full max-h-96"
+          />
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg mt-8"
+            onClick={() => setSelectedImage(null)}
+          >
+            Remove Image
+          </button>
+        </div>
+      ) : (
+        <div className="border-4 border-dashed border-gray-400 rounded-lg p-8">
+          <h2 className="text-xl font-bold mb-4">Select an Image to Upload</h2>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            className="py-2"
+          />
+        </div>
+      )}
     </div>
   );
 }
