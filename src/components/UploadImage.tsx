65cc8a98f26af710ec423a4d18a24866163ab555
hqunted <berkekirci@gmail.com>
2023-04-08 11:38:30 +0200

    feat: rearrange UploadImage

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -6,7 +6,7 @@ export const UploadImage = ({ handleImageUpload }: UploadImageProps) => {
   return (
     <>
       <div className="absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 bg-gradient-to-br from-blue-300 to-blue-600 w-1/2 h-1/2 rounded-lg shadow-lg"></div>
-      <label className="animate-bounce bg-white text-blue-500 px-4 py-2 rounded-lg mt-8 hover:bg-blue-500  hover:text-white cursor-pointer">
+      <label className="px-4 py-2 mt-[15%] rounded-lg bg-white text-blue-500 animate-bounce hover:bg-blue-500  hover:text-white cursor-pointer">
         Upload Image
         <input
           type="file"
