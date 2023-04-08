ff6401ab655acbd6125b78702a143eb4580d75f1
hqunted <berkekirci@gmail.com>
2023-04-08 11:04:41 +0200

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="p-4 pl-[4%] pt-[4%] flex items-center space-x-2 text-indigo-900 font-bold text-6xl font-amaticsc drop-shadow-lg transform  transition duration-300">
+      <div>Triangle Analyzer</div>
+    </div>
+  );
+};
