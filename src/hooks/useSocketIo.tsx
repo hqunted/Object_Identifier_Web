44a21bc01686eb2ae9e48fa895c6cfaed4c3a8a0
hqunted <berkekirci@gmail.com>
2023-04-28 16:34:54 +0200

    feat: add useSocketIo hook

--- src/hooks/useSocketIo.tsx
+++ src/hooks/useSocketIo.tsx
@@ -1,13 +1,41 @@
+import { socket } from "../socket";
+import { useState, useEffect } from "react";
+
 const useSocketIo = () => {
-  // // const [imageData, setImageData] = useState("");
-  // useEffect(() => {
-  //   socket.on("connect", () => {
-  //     console.log("Connected to server");
-  //   });
-  //   socket.on("imageData", (data) => {
-  //     console.log("Received image data from server");
-  //   });
-  // }, []);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [recieveImageData, setRecieveImageData] = useState("");
+
+  const base64ToImage = (base64: string) => {
+    return `data:image/png;base64,${base64}`;
+  };
+  console.log(base64ToImage(recieveImageData));
+  socket.on("imageData", (imageData) => {
+    setRecieveImageData(imageData);
+  });
+
+  const changeVisibilityOnLoad = () => {
+    return recieveImageData ? "opacity-100" : "opacity-0";
+  };
+
+  useEffect(() => {
+    const onConnect = () => {
+      setIsConnected(true);
+      console.log("Connected!");
+    };
+
+    const onDisconnect = () => {
+      setIsConnected(false);
+      console.log("Disconnected!");
+    };
+
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+  return { recieveImageData, base64ToImage, changeVisibilityOnLoad };
 };
 
 export default useSocketIo;
