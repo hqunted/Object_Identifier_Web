c4c7c7032db78255822767720b453d664f22eb09
hqunted <berkekirci@gmail.com>
2023-04-28 11:32:07 +0200

    feat: reinitialize socket.io

--- src/pages/ResultImage.tsx
+++ src/pages/ResultImage.tsx
@@ -1,45 +1,54 @@
 import { useEffect } from "react";
 import { ImageFrame } from "../components/ImageFrame";
-import { RecieveImage } from "../services/processImage";
 import { socket } from "../socket";
 import { useState } from "react";
+import { LoadingModal } from "../components/LoadingModal";
 
 export interface selectedImage {
   selectedImage: string | undefined;
 }
-const recievedImg = RecieveImage();
 
 export const ResultImage = ({ selectedImage }: selectedImage) => {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
+  const [recieveImageData, setRecieveImageData] = useState("");
 
-  useEffect(() => {
-    console.log("SAISJFLKSAFKFSAL");
-    console.log(isConnected);
-  }, [recievedImg]);
-
+  const base64ToImage = (base64: string) => {
+    return `data:image/png;base64,${base64}`;
+  };
+  console.log(base64ToImage(recieveImageData));
+  socket.on("imageData", (imageData) => {
+    setRecieveImageData(imageData);
+  });
   useEffect(() => {
     function onConnect() {
       setIsConnected(true);
+      console.log("Connected!");
     }
 
     function onDisconnect() {
       setIsConnected(false);
+      console.log("Disconnected!");
     }
+
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-full ">
       <div className="w-full max-w-screen-lg flex flex-col md:flex-col lg:flex-row justify-center items-center relative">
-        <div className="flex justify-center lg:pt-4xs lg:w-2.5xl lg:rounded-3xl">
-          <ImageFrame imageData={"https://picsum.photos/200/300"} />
-        </div>
         {selectedImage && (
           <div className="flex justify-center lg:pt-4xs lg:w-2.5xl lg:rounded-3xl">
             <ImageFrame imageData={selectedImage} />
           </div>
         )}
-
+        <div className="flex justify-center lg:pt-4xs lg:w-2.5xl lg:rounded-3xl">
+          <ImageFrame imageData={base64ToImage(recieveImageData)} />
+        </div>
         <div className="bg-gradient-to-br from-blue-300 to-blue-600 rounded-3xl" />
       </div>
     </div>
