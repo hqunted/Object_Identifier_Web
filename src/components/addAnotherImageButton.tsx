8e59d2e1d69e158b913062afd641ee6bdc77ddb4
hqunted <berkekirci@gmail.com>
2023-05-03 10:41:32 +0200

    feat: made newImageButton responsive

--- src/components/addAnotherImageButton.tsx
+++ src/components/addAnotherImageButton.tsx
@@ -2,7 +2,7 @@ export const addAnotherImageButton = (setSelectedImage: Function) => {
   return (
     <div>
       <button
-        className="z-49 absolute flex rounded-b-lg 2xl:text-sm xl:text-xs 2xl:p-3xs xl:p-xs lg:p-sm bg-gradient-to-br from-blue-300 to-blue-600 lg:text-2xl md:text-4xl md:p-sm text-white hover:bg-white hover:scale-110 cursor-pointer"
+        className="z-49 absolute flex rounded-b-lg 2xl:text-sm xl:text-xs 2xl:p-2xs 2xl:max-h-2.5xl xl:max-h-2.5xl lg:max-h-2xl md:max-h-2.5x lg:text-2xl md:text-xl md:p-3xs bg-gradient-to-br from-blue-300 to-blue-600 text-white hover:bg-white hover:scale-110 cursor-pointer"
         onClick={() => {
           setSelectedImage(null);
         }}
